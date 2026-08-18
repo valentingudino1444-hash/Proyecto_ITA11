65431f8f8f04d25a997f13d0ff2c71f3a730ab0d
FranciscoCarreon05 <2023348031@uteq.edu.mx>
2026-08-17 22:05:44 -0600

    Add pantalla.h header file for LCD display functions

--- Proyecto2/pantalla.h
+++ Proyecto2/pantalla.h
@@ -1,1 +1,15 @@
+#ifndef PANTALLA_H_
+#define PANTALLA_H_
 
+#include <stdint.h>
+
+#define LCD_ADDR 0x4E // O probar con 0x7E si no muestra nada
+
+void Pantalla_Init(void);
+void Pantalla_Comando(uint8_t cmd);
+void Pantalla_Caracter(uint8_t data);
+void Pantalla_Cadena(char *str);
+void Pantalla_Cursor(uint8_t fila, uint8_t columna);
+void Pantalla_Limpiar(void);
+
+#endif /* PANTALLA_H_ */
